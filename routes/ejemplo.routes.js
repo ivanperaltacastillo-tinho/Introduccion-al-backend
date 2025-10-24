3a2b49ba1d0495fe8cc00a7ffa7b029a8ace4e44
ivanperaltacastillo-tinho <ivan.peralta.castillo@utelvt.edu.ec>
2025-10-23 19:14:22 -0500

    Primer commit

---
+++ routes/ejemplo.routes.js
@@ -0,0 +1,44 @@
+import {Router} from 'express';
+const ejemplo= Router();
+
+ejemplo.get('/', (req, res) => {
+    res.json({
+        msg: 'get all API'
+    })
+});
+
+ejemplo.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    res.json({
+        msg: 'get all API',
+        id
+    })
+});
+
+ejemplo.put('/', (req, res) => {
+    const body = req.body;
+
+    res.json({
+        msg: 'put API',
+        body
+    })
+});
+
+ejemplo.post('/', (req, res) => {
+    const body = req.body;
+
+    res.json({
+        msg: 'post API',
+        body
+    })
+});
+
+ejemplo.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({
+        msg: 'delete API'
+    })
+});
+
+export default ejemplo;
